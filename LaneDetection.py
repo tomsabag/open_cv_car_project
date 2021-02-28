6eefd0e9b7463f48605dca8f042ce578b12d2dfb
tomsabag <63912484+tomsabag@users.noreply.github.com>
2021-02-28 22:11:56 +0200

    include motor module & update requirements.txt

--- LaneDetection.py
+++ LaneDetection.py
@@ -21,28 +21,28 @@ utils.initialize_Trackbars(initial_points_trackbar_values)
 
 def lane_curve(frame):
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    frame_copy = frame.copy()
-
-    threshed_frame = utils.thresholding(frame)
+    points = utils.val_trackbars()
     height, width, channels = 240, 480, 3  # frame.shape
 
-    points = utils.val_trackbars()
-    warped_frame = utils.warp(frame_copy, points, width, height)
+    # create frames
+    threshed_frame = utils.thresholding(frame)
     warped_and_threshed_frame = utils.warp(threshed_frame, points, width, height)
-    warped_frame_with_points = utils.draw_points(warped_frame, points)
     frame_with_points = utils.draw_points(frame.copy(), points)
+    # create mid histogram point
     middle_point, hist_image_quarter = utils.get_histogram(warped_and_threshed_frame, min_thresh_percent=0.5,
                                                  display=True, region_percentage=4)
     curve_avg_point, hist_image_all = utils.get_histogram(warped_and_threshed_frame, min_thresh_percent=0.9,
                                                  display=True)
     curve_raw = curve_avg_point - middle_point
 
+    # curve calculation
     curve_list.append(curve_raw)
     if len(curve_list) > curve_list_avg_len:
         curve_list.pop(0)
     curve = sum(curve_list) // len(curve_list)
     hist_image_quarter = utils.display_curve(hist_image_quarter, curve_raw, fps)
 
+    # visualize frames
     frames_stack1 = cv2.hconcat([hist_image_quarter, hist_image_all, frame_with_points])
     frames_stack2 = cv2.hconcat([threshed_frame, warped_and_threshed_frame])
 
--- try.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-print(np.sum([[1,2,3], [10, 20, 30]], axis=1))
